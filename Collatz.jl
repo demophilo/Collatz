53e649e1b6e8ff2756fdf91150bc5eddaf7631b7
Karl Linek <karl@linek.at>
2023-12-01 10:47:54 +0100

    typed variables to Int128

--- Collatz.jl
+++ Collatz.jl
@@ -6,23 +6,27 @@ Collatz:
 =#
 
 
-function collatz_sequence_length(n)
-    m::Int128 = n 
-    count = 0
+
+
+function collatz_sequence_length(n::Int128)
+    m::Int128 = n
+    count = 0  # Die Variable count ist nicht explizit als BigInt definiert
+
     while m != 1
         m = ((m << 1) + m + 1) * (m & 1 != 0) + (m >> 1) * (m & 1 == 0)
         count = count + (m & 1 != 0)
     end
+
     return count
 end
 
 function max_sequence_length_in_given_collatz_length(binary_digits)
-    longest_sequence_number1 = 0
-    longest_sequence_number2 = 0
+    longest_sequence_number1 = []
+    longest_sequence_number2 = []
     longest_sequence = 0
-    lower_limit = 2 << (binary_digits - 2) + 1
-    upper_limit = 2 << (binary_digits - 1) - 1
-    for i in lower_limit:2:upper_limit
+    lower_limit::Int128 = 2 << (binary_digits - 2) + 1
+    upper_limit::Int128 = 2 << (binary_digits - 1) - 1
+    for i::Int128 in lower_limit:2:upper_limit
         sequence_length = collatz_sequence_length(i)
         if sequence_length > longest_sequence
             longest_sequence = sequence_length
@@ -38,9 +42,9 @@ end
 
 maxDigit = 40
 
-
-
+println(max_sequence_length_in_given_collatz_length(41))
+#=
 for i in 2:maxDigit
     println(i," ", max_sequence_length_in_given_collatz_length(i))
 end
-
+=#
