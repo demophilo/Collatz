4c38735e360a44072c3b299b278c6a11f026353b
Karl Linek <karl@linek.at>
2024-11-03 11:24:32 +0100

    generated julia project

---
+++ src/Collatz_julia.jl
@@ -0,0 +1,14 @@
+module Collatz_julia
+
+export collatz_operator
+
+function collatz_operator(zahl::UInt128)::UInt128
+	n = 0
+	while (zahl & 1) == 0
+		n += 1
+		zahl >>= 1
+	end
+	return ((zahl << 1) + zahl + 1) << n
+end
+
+end # module Collatz_julia
