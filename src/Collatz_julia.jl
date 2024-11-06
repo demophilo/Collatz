c68601a7b3bdf451a9f13f51fc3dea660ca3427a
Karl Linek <karl@linek.at>
2024-11-06 21:25:31 +0100

    added function collatz_sequence_investigation

--- src/Collatz_julia.jl
+++ src/Collatz_julia.jl
@@ -1,6 +1,6 @@
 module Collatz_julia
 
-export collatz_operator, collatz_sequence_length, max_sequence_length_in_given_collatz_length
+export collatz_operator, collatz_sequence_length, max_sequence_length_in_given_collatz_length, collatz_sequence_investigation
 
 function collatz_operator(zahl::UInt128)::UInt128
 	n = 0
@@ -40,6 +40,22 @@ function max_sequence_length_in_given_collatz_length(collatz_length)
         end
     end
     return (longest_sequence_number1, longest_sequence_number2)
+end
+
+function collatz_sequence_investigation(num::UInt128)
+	m::UInt128 = num
+    count_c_operator = 0
+	count_d_operator = 0
+
+    while m != 1
+		count_c_operator = count_c_operator + (m & 1)
+		count_d_operator = count_d_operator + (m & 1 != 0)
+        m = (m & 1 != 0) * ((m << 1) + m + 1) + (m >> 1) * (m & 1 == 0)
+    end
+
+	
+
+    return count_c_operator, count_d_operator
 end
 end # module Collatz_julia
 #=
