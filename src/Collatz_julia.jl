91ce031d26872d0be36c60162ef8314d76a7a8a7
Karl Linek <karl@linek.at>
2024-11-06 20:39:04 +0100

    cleanup after benchmarks

--- src/Collatz_julia.jl
+++ src/Collatz_julia.jl
@@ -1,6 +1,6 @@
 module Collatz_julia
 
-export collatz_operator, collatz_sequence_length
+export collatz_operator, collatz_sequence_length, max_sequence_length_in_given_collatz_length
 
 function collatz_operator(zahl::UInt128)::UInt128
 	n = 0
@@ -22,4 +22,29 @@ function collatz_sequence_length(n::UInt128)
 
     return count
 end
+
+function max_sequence_length_in_given_collatz_length(collatz_length)
+    longest_sequence_number1 = []
+    longest_sequence_number2 = []
+    longest_sequence = 0
+    lower_limit::UInt128 = 2 << (collatz_length - 2) + 1
+    upper_limit::UInt128 = 2 << (collatz_length - 1) - 1
+    for i::UInt128 in lower_limit:2:upper_limit
+        sequence_length = collatz_sequence_length(i)
+        if sequence_length > longest_sequence
+            longest_sequence = sequence_length
+            longest_sequence_number1 = [i, sequence_length]
+            longest_sequence_number2 = [0,0];
+        elseif sequence_length == longest_sequence
+            longest_sequence_number2 = [i, sequence_length]
+        end
+    end
+    return (longest_sequence_number1, longest_sequence_number2)
+end
 end # module Collatz_julia
+#=
+using .Collatz_julia
+
+
+@btime max_sequence_length_in_given_collatz_length(27)
+=#
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,14 @@ include("../src/Collatz_julia.jl")
 using .Collatz_julia
 
 @testset "Collatz" begin
-    @test collatz_operator(UInt128(5)) == 16
-    @test collatz_operator(UInt128(10)) == 32
-   
+	@test collatz_operator(UInt128(5)) == 16
+	@test collatz_operator(UInt128(10)) == 32
+
 end
 
 @testset "collatz_sequence_length" begin
-    @test collatz_sequence_length(UInt128(5)) == 1
-    @test collatz_sequence_length(UInt128(10)) == 1
-    @test collatz_sequence_length(UInt128(3)) == 2
+	@test collatz_sequence_length(UInt128(5)) == 1
+	@test collatz_sequence_length(UInt128(10)) == 1
+	@test collatz_sequence_length(UInt128(3)) == 2
 end
+
